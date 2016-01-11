177a3c41046646b0c4bf9539133f604748c5e374
joksina <jksalaudeen@yahoo.com>
2016-01-10 16:00:23 -0800

    made some changes to the redirect

--- client/app/app.js
+++ client/app/app.js
@@ -35,10 +35,10 @@ window.fbAsyncInit = function() {
   });
 
   FB.Event.subscribe('auth.login', function(resp) {
-   window.location = 'http://www.localhost:3000/#/home';
+   window.location = 'https://chat-geo.herokuapp.com/#/home';
  });
     FB.Event.subscribe('auth.logout', function(resp) {
-   window.location = 'http://www.localhost:3000/';
+   window.location = 'https://chat-geo.herokuapp.com/';
  });
 
 };
