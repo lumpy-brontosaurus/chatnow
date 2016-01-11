fb4f5848d2d64a04af52fb83245b9b675ba6501a
joksina <jksalaudeen@yahoo.com>
2016-01-11 14:20:14 -0800

    changing the usernme

--- client/app/app.js
+++ client/app/app.js
@@ -95,7 +95,7 @@ app.controller('AuthCtrl', ["$scope", "User", function ($scope, User) {
                     User.getUser()
                          .then(function (resData){
                              console.log(resData[1].user);
-                             username = resData[1].user;
+                             // username = resData[1].user;
                              console.log(resData);
                         })
                         .catch(function (error){
