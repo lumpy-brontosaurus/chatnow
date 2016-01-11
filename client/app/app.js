38deb6683f99f499d1afa882c526194732ebabcb
joksina <jksalaudeen@yahoo.com>
2016-01-10 18:05:49 -0800

    some changes

--- client/app/app.js
+++ client/app/app.js
@@ -1,20 +1,17 @@
 
-var access_token;
 var app = angular.module('geoChat', ['ui.router', 'ngCookies', 'ngResource', 'ngSanitize','btford.socket-io'])
     .value('nickName', username);
 
+var access_token;
 var username = '';
 
 function statusChangeCallback(response) {
     if (response.status === 'connected') {
-
-
-
-     FB.api('/me', function(response) {
+    FB.api('/me', function(response) {
         console.log("here");
             console.log(JSON.stringify(response));
             username = response.name;
-});  
+    });
    }
   }
 
@@ -84,6 +81,7 @@ $scope.FBLogin = function(){
         $scope.$apply(function() {
           $scope.myFriends = response.data;
           console.log(response);
+          username = response.name;
         });
       });
     };
