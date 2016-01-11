6d2d8dc8e82bcf8bfc7a944ae9a7835453d99285
joksina <jksalaudeen@yahoo.com>
2016-01-11 10:35:58 -0800

    made some changes on the routes.js

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 //app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-
+require('./config/routes.js');
 // for dev
 app.use(express.static(__dirname +'/../client'));
 
