e345176f1ad9ef67f9f9953c2da341f6ac209484
joksina <jksalaudeen@yahoo.com>
2016-01-11 22:05:56 -0800

    more changes

--- server/sockets/base.js
+++ server/sockets/base.js
@@ -1,18 +1,11 @@
 module.exports = function (io) {
-    'use strict';
-    io.on('connection', function (socket) {
-        socket.on('message', function (from, msg) {
-
-            // console.log('recieved message from',
-            //     from, 'msg', JSON.stringify(msg));
-
-            // console.log('broadcasting message');
-            // console.log('messageLoad is', msg);
-            io.sockets.emit('broadcast', {
-                messageLoad: msg,
-                source: from
-            });
-            console.log('broadcast complete');
-        });
+  'use strict';
+  io.on('connection', function (socket) {
+    socket.on('message', function (from, msg) {
+        io.sockets.emit('broadcast', {
+          messageLoad: msg,
+          source: from
+      });
     });
+  });
 };
